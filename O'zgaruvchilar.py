0ae4c61d843cb6d8422db96d1b1ccf980ba5635e
BaxodirNapasov <baxodirnapasov@gmail.com>
2025-01-14 14:54:42 +0500

    Create O'zgaruvchilar.py

---
+++ O'zgaruvchilar.py
@@ -0,0 +1,25 @@
+# Muallif:Baxodir Napasov
+# Sana: 12.01.2025
+# pytho_n
+
+ozgaruvchi = "malumotlar saqlovchi"
+print("o'zgaruvchi sfatida ishlatib bo'lmaydigan so'zlar:")
+print("false,class,from,or\nNane,continue,global,pass\nTrue,def,if,raise\nand,del,import,return\nas,elif,in,try\nassert,else,is,while\nasync,except,lambda,with\nawait,finally,nonlocal,yield\nbreak,for,not.")
+
+dasturlar = "python\nspyder\nanaconda"
+print(dasturlar)
+
+matn = "yangi ma'lumotlar,yangi ilmlar"
+print(matn)
+print("hello world")
+
+xabar = (25*3)
+print(xabar)
+
+qoldiq_olish = (15%6)#ya'ni bunda 15 oltiga bo'linib qolgani qoldiq sifatida olinadi
+print(qoldiq_olish)
+
+
+qwqq = "uchning kvadrati",(3**3),"ga teng"
+print (qwqq)
+

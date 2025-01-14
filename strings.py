5eccd34d916abead85f8821e8c792835c404f65b
BaxodirNapasov <baxodirnapasov@gmail.com>
2025-01-14 15:18:55 +0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,112 @@
+# string=matn bilan ishlash
+#Created on Mon Jan 13 09:27:52 2025
+
+#ism = "Baxodir"
+#familiya = "Napasov"
+#ism_familiya=f"{ism}  {familiya}"
+#print(ism_familiya)
+
+#print (f"Salom,mening ismim {ism}.{ism} {familiya}!")
+
+#bugun = "mening ko'chadan 2500 💵 qarzim bor. " 
+#print(bugun)
+
+#print(ism+' '+familiya)
+
+#print(ism_familiya.upper())
+
+#print(ism_familiya.lower())
+
+#print(ism_familiya.title())
+
+#print(ism_familiya.capitalize())
+
+#meva = "             olma            "   
+
+#print("men " + meva.lstrip()+" sharbatini yaxshi ko'raman")
+
+#print("men "+meva.rstrip()+" sharbatni yaxshi ko'raman")
+
+#olma = ("Men "+meva.strip()+" sharbatini yaxshi ko'raman")
+
+#print(olma.capitalize())
+
+#ism=input("ismingiz nima?")
+#print(ism.title())
+
+#familiya=input("familiyangiz?")
+#print(familiya.title())
+
+#sharif=input("sharifingiz?")
+#print(sharif.title())
+
+#malumot= f"{ism}\n{familiya}\n{sharif}"
+
+#print(malumot.title())
+
+#viloyat=input("viloyat\n")
+#print(viloyat.upper())
+
+#shahar=input("shahar\n")
+#print(shahar.upper())
+
+#tuman=input("tuman\n")
+#print(tuman.upper())
+
+#kocha=input("ko'cha\n")
+#print(kocha.upper())
+
+#uy=input("uy\n")
+#print(uy.upper())
+
+#tel=input("telefon:\n")
+#print(tel)
+ 
+#manzil=input(input(viloyat.upper())+ input(shahar.upper())+ input(tuman.upper())+input(kocha.upper())+input(uy.upper())+input(tel.upper()).upper())     
+#print(manzil)
+     
+
+#manzil=(f"{viloyat} {shahar} {tuman} {kocha} {uy}".upper)
+
+#print(manzil.upper())
+
+#shahar = "toshkent"
+#tuman  = "yunusobod"
+#kocha  = "chinobod"
+#mahalla = "otchopar"
+
+#manzil= (f"men,{shahar} shahri,\n{tuman} tumani,\n{kocha} ko'chasi,\n{mahalla}  mahallasida  yashayman.".upper())
+
+#print(manzil.upper())
+
+#print(manzil.lower())
+
+#print(manzil.title())
+
+#print(manzil.capitalize())
+
+
+#t_yil = int(input("Tug'ilgan yilingizni kiriting"))
+
+#yosh = 2024-t_yil
+
+#print("siz" " "+str(yosh) +" " "yoshda ekansiz")
+
+print("siz quyida +,-,*,|, amallarini bajarishingiz mumkin.")
+
+qo = int(input("Qiymat kiriting"))
+q = int(input("Ikkinchini kiriting"))
+
+
+print ( f" {qo}+{q}=",qo+q )
+print ( f" {qo}-{q}=",qo-q )
+print ( f" {qo}*{q}=",qo*q )
+print ( f" {qo}/{q}=",qo//q )     #bo'lish amalidan butun natija olish uchun\
+  #  ( // ) belgi qoyiladi
+
+
+
+
+
+
+
